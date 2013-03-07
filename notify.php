5a45627ff1ee9239affa5fb472b6ddef91f5f865
Andrew Champ <atmydarkest@gmail.com>
2013-03-07 14:03:05 -0500

    Cleaned up tabbing

--- notify.php
+++ notify.php
@@ -31,59 +31,59 @@
 
 class notify{
   
-		public $url;
-		public $match;
-		public $email;
-		public $subject;
-		public $matched;
+	public $url;
+	public $match;
+	public $email;
+	public $subject;
+	public $matched;
 		
-		private $result;
+	private $result;
 	
 		
-		public function __construct($_url, $_match, $_email, $_subject){
-			$this->url = $_url;
-			$this->match = $_match;
-			$this->email = $_email;
-			$this->subject = $_subject;
-			$this->scrape();
-			$this->search();
-		}
+	public function __construct($_url, $_match, $_email, $_subject){
+		$this->url = $_url;
+		$this->match = $_match;
+		$this->email = $_email;
+		$this->subject = $_subject;
+		$this->scrape();
+		$this->search();
+	}
 		
 		
-		public function scrape(){
-			$ch = curl_init($this->url);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			$result = curl_exec($ch);
-			$this->result = $this->clean($result);
-			curl_close($ch);
-		}
+	public function scrape(){
+		$ch = curl_init($this->url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		$this->result = $this->clean($result);
+		curl_close($ch);
+	}
 		
 		
-		public function search(){
-			$getit = preg_match("/".$this->match."/i", $this->result, $matches);
-			$this->matched = $getit;
-		}
+	public function search(){
+		$getit = preg_match("/".$this->match."/i", $this->result, $matches);
+		$this->matched = $getit;
+	}
 		
 		
-		public function mailer(){
-			$e_body = $this->url." \r\n\n";
-			$e_content = "\r\n\n";    
-			$e_reply = "";
-			$headers = "From: ".$this->email." \r\nReply-To: ".$this->email." \r\nReturn-Path: ".$this->email." \r\n";
-			$msg = $e_body . $e_content . $e_reply;
-			if(mail($this->email, $this->subject, $msg, $headers)):	
-				 return true;
-			else:
-				return false;
-			endif;
-		}
+	public function mailer(){
+		$e_body = $this->url." \r\n\n";
+		$e_content = "\r\n\n";    
+		$e_reply = "";
+		$headers = "From: ".$this->email." \r\nReply-To: ".$this->email." \r\nReturn-Path: ".$this->email." \r\n";
+		$msg = $e_body . $e_content . $e_reply;
+		if(mail($this->email, $this->subject, $msg, $headers)):	
+			 return true;
+		else:
+			return false;
+		endif;
+	}
 		
 		
-		private function clean($str){
-			return strip_tags($str);
-		}
+	private function clean($str){
+		return strip_tags($str);
+	}
 	
 }
 
