e0b9f33cada8c619ebe4f5caf80cb9d23433c30d
Andrew Champ <atmydarkest@gmail.com>
2013-03-07 14:50:26 -0500

    Added User Agent String to the cURL request

--- notify.php
+++ notify.php
@@ -55,6 +55,7 @@ class notify{
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17');
 		$result = curl_exec($ch);
 		$this->result = $this->clean($result);
 		curl_close($ch);
